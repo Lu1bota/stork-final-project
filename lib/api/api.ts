0c2cc16fb62d3b301b4173ed464696236621b3e4
Danylo Liubota <daniil.lyubota@gmail.com>
2025-09-25 20:45:42 +0300

    fix: instance

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
-  withCredentials: false,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
