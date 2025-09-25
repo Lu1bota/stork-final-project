b02597d91b8560abe9d320662a8a43e6660ecda6
Danylo Liubota <daniil.lyubota@gmail.com>
2025-09-25 19:10:34 +0300

    feat: add auth provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,3 @@
-"use client";
-
-import React from "react";
-import TasksReminderCard from "../components/dashboard/TasksReminderCard/TasksReminderCard";
-import FeelingCheckCard from "../components/dashboard/FeelingCheckCard/FeelingCheckCard";
-import styles from "./page.module.css";
-import MomTipCard from "@/components/dashboard/MomTipCard/MomTipCard";
-
 export default function DashboardPage() {
-  // Заглушки для открытия модального окна
-  const openAddTaskModal = () => {
-    console.log("Открыть модалку для добавления задачи");
-  };
-
-  const openAddDiaryModal = () => {
-    console.log("Открыть модалку для добавления записи в дневник");
-  };
-
-  return (
-    <main className={styles.container}>
-      <div className={styles.grid}>
-        <div className={styles.leftColumn}>
-          <TasksReminderCard onOpenAddTaskModal={openAddTaskModal} />
-        </div>
-        <div className={styles.rightColumn}>
-          <FeelingCheckCard onOpenAddDiaryEntryModal={openAddDiaryModal} />
-        </div>
-      </div>
-      <MomTipCard tip="hello" />
-    </main>
-  );
+  return null;
 }
