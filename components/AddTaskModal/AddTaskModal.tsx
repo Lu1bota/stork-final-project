4eed58c30139ae882b49e47523b6b064fd0d7b96
OlhaYakym <olhayakym@gmail.com>
2025-09-29 13:17:12 +0200

    Fix_AddTaskModal

--- components/AddTaskModal/AddTaskModal.tsx
+++ components/AddTaskModal/AddTaskModal.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { useState } from "react";
 import Modal from "../Modal/Modal";
 import AddTaskForm from "./AddTaskForm";
-import ConfirmationModal from "@/components/ConfirmationModal/ConfirmationModal";
 
 interface AddTaskModalProps {
   isEdit?: boolean;
@@ -18,38 +16,17 @@ export default function AddTaskModal({
   onClose,
   onSubmit,
 }: AddTaskModalProps) {
-  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
-
-  const handleClose = () => setIsConfirmationModalOpen(true);
-
-  const handleConfirmCancel = () => {
-    setIsConfirmationModalOpen(false);
-    onClose();
-  };
-
   const handleSubmit = async (values: { title: string; date: Date }) => {
     await onSubmit(values);
     onClose();
   };
 
   return (
-    <>
-      <Modal
-        title={isEdit ? "Редагувати завдання" : "Нове завдання"}
-        onClose={handleClose}
-      >
-        <AddTaskForm initialValues={initialValues} onSubmit={handleSubmit} />
-      </Modal>
-
-      {isConfirmationModalOpen && (
-        <ConfirmationModal
-          title="Ви точно хочете вийти?"
-          confirmButtonText="Так"
-          cancelButtonText="Ні"
-          onConfirm={handleConfirmCancel}
-          onCancel={() => setIsConfirmationModalOpen(false)}
-        />
-      )}
-    </>
+    <Modal
+      title={isEdit ? "Редагувати завдання" : "Нове завдання"}
+      onClose={onClose}
+    >
+      <AddTaskForm initialValues={initialValues} onSubmit={handleSubmit} />
+    </Modal>
   );
 }
