f644eaf1da7872427082e4e15e9b70d4b3fb88ab
Max Akimov <maxymus2828@gmail.com>
2025-10-01 12:20:51 +0200

    cleaned unnecesary title tag

--- components/Diary/AddDiaryEntryForm/AddDiaryEntryForm.tsx
+++ components/Diary/AddDiaryEntryForm/AddDiaryEntryForm.tsx
@@ -162,142 +162,139 @@ export const AddDiaryEntryForm = ({
         enableReinitialize={true}
       >
         {({ errors, touched, isSubmitting, values, setFieldValue }) => (
-          <>
-            <h3 className={css.from_modal_title}>Новий запис</h3>
-            <Form className={css.form}>
-              <div className={css.fieldWrap}>
-                <label className={css.title} htmlFor="title">
-                  Заголовок
-                </label>
-                <Field
-                  className={`${css.input} ${errors.title && touched.title ? css.inputError : ""}`}
-                  type="text"
-                  name="title"
-                  id="title"
-                  placeholder="Введіть заголовок запису"
-                />
-                <ErrorMessage
-                  name="title"
-                  component="div"
-                  className={css.error}
-                />
-              </div>
+          <Form className={css.form}>
+            <div className={css.fieldWrap}>
+              <label className={css.title} htmlFor="title">
+                Заголовок
+              </label>
+              <Field
+                className={`${css.input} ${errors.title && touched.title ? css.inputError : ""}`}
+                type="text"
+                name="title"
+                id="title"
+                placeholder="Введіть заголовок запису"
+              />
+              <ErrorMessage
+                name="title"
+                component="div"
+                className={css.error}
+              />
+            </div>
 
-              {/* Поле Категорії */}
-              <div className={css.fieldWrap} ref={dropdownRef}>
-                <label className={css.title}>Категорії</label>
-                {!isDropdownOpen && values.emotions.length > 0 && (
-                  <div
-                    className={`${css.input} ${css.selectedTagsContainer} ${errors.emotions && touched.emotions ? css.inputError : ""}`}
-                    onClick={handleDropdownClick}
-                    ref={dropdownInputRef}
-                  >
-                    {values.emotions.map((emotionId) => {
-                      const emotion = emotionsData?.find(
-                        (e) => e._id === emotionId
-                      );
-                      return (
-                        <div key={emotionId} className={css.selectedEmotionTag}>
-                          {emotion?.title}
-                        </div>
-                      );
-                    })}
-                    <svg
-                      width={24}
-                      height={24}
-                      className={`${css.dropdownArrow} ${isDropdownOpen ? css.dropdownArrowOpen : ""}`}
-                    >
-                      <use href="/sprite.svg#chevron_right"></use>
-                    </svg>
-                  </div>
-                )}
-
-                {/* Поле вибору (немає тегів/список відкритий) */}
-                {(isDropdownOpen || values.emotions.length === 0) && (
-                  <div
-                    className={`${css.input} ${isDropdownOpen ? css.inputOpen : ""} ${errors.emotions && touched.emotions ? css.inputError : ""}`}
-                    onClick={handleDropdownClick}
-                    ref={dropdownInputRef}
-                  >
-                    <span
-                      className={`${css.placeholder} ${errors.emotions && touched.emotions ? css.placeholderError : ""}`}
-                    >
-                      Оберіть категорію
-                    </span>
-                    <svg
-                      width={24}
-                      height={24}
-                      className={`${css.dropdownArrow} ${isDropdownOpen ? css.dropdownArrowOpen : ""}`}
-                    >
-                      <use href="/sprite.svg#chevron_right"></use>
-                    </svg>
-                  </div>
-                )}
-
-                {/* Випадаючий список */}
-                {isDropdownOpen && (
-                  <div className={css.emotionsDropdownList}>
-                    {emotionsData?.map((emotion) => (
-                      <div
-                        key={emotion._id}
-                        className={css.emotionItem}
-                        onClick={() =>
-                          toggleEmotion(emotion._id, setFieldValue, values)
-                        }
-                      >
-                        <div
-                          className={`${css.checkbox} ${values.emotions.includes(emotion._id) ? css.checked : ""}`}
-                        >
-                          {values.emotions.includes(emotion._id) && (
-                            <svg width={14} height={11}>
-                              <use href="/sprite.svg#flag"></use>
-                            </svg>
-                          )}
-                        </div>
-                        <span>{emotion.title}</span>
+            {/* Поле Категорії */}
+            <div className={css.fieldWrap} ref={dropdownRef}>
+              <label className={css.title}>Категорії</label>
+              {!isDropdownOpen && values.emotions.length > 0 && (
+                <div
+                  className={`${css.input} ${css.selectedTagsContainer} ${errors.emotions && touched.emotions ? css.inputError : ""}`}
+                  onClick={handleDropdownClick}
+                  ref={dropdownInputRef}
+                >
+                  {values.emotions.map((emotionId) => {
+                    const emotion = emotionsData?.find(
+                      (e) => e._id === emotionId
+                    );
+                    return (
+                      <div key={emotionId} className={css.selectedEmotionTag}>
+                        {emotion?.title}
                       </div>
-                    ))}
-                  </div>
-                )}
-                <ErrorMessage
-                  name="emotions"
-                  component="div"
-                  className={css.error}
-                />
-              </div>
+                    );
+                  })}
+                  <svg
+                    width={24}
+                    height={24}
+                    className={`${css.dropdownArrow} ${isDropdownOpen ? css.dropdownArrowOpen : ""}`}
+                  >
+                    <use href="/sprite.svg#chevron_right"></use>
+                  </svg>
+                </div>
+              )}
 
-              <div className={css.fieldWrap}>
-                <label className={css.title} htmlFor="description">
-                  Запис
-                </label>
-                <Field
-                  className={`${css.textarea} ${errors.description && touched.description ? css.inputError : ""}`}
-                  as="textarea"
-                  name="description"
-                  id="description"
-                  rows={5}
-                  placeholder="Запишіть, як ви себе відчуваєте"
-                />
-                <ErrorMessage
-                  name="description"
-                  component="div"
-                  className={css.error}
-                />
-              </div>
+              {/* Поле вибору (немає тегів/список відкритий) */}
+              {(isDropdownOpen || values.emotions.length === 0) && (
+                <div
+                  className={`${css.input} ${isDropdownOpen ? css.inputOpen : ""} ${errors.emotions && touched.emotions ? css.inputError : ""}`}
+                  onClick={handleDropdownClick}
+                  ref={dropdownInputRef}
+                >
+                  <span
+                    className={`${css.placeholder} ${errors.emotions && touched.emotions ? css.placeholderError : ""}`}
+                  >
+                    Оберіть категорію
+                  </span>
+                  <svg
+                    width={24}
+                    height={24}
+                    className={`${css.dropdownArrow} ${isDropdownOpen ? css.dropdownArrowOpen : ""}`}
+                  >
+                    <use href="/sprite.svg#chevron_right"></use>
+                  </svg>
+                </div>
+              )}
 
-              <button
-                className={css.button}
-                type="submit"
-                disabled={isSubmitting}
-              >
-                {isSubmitting
-                  ? isEditing
-                    ? "Оновлення..."
-                    : "Збереження..."
-                  : "Зберегти"}
-              </button>
-            </Form>
-          </>
+              {/* Випадаючий список */}
+              {isDropdownOpen && (
+                <div className={css.emotionsDropdownList}>
+                  {emotionsData?.map((emotion) => (
+                    <div
+                      key={emotion._id}
+                      className={css.emotionItem}
+                      onClick={() =>
+                        toggleEmotion(emotion._id, setFieldValue, values)
+                      }
+                    >
+                      <div
+                        className={`${css.checkbox} ${values.emotions.includes(emotion._id) ? css.checked : ""}`}
+                      >
+                        {values.emotions.includes(emotion._id) && (
+                          <svg width={14} height={11}>
+                            <use href="/sprite.svg#flag"></use>
+                          </svg>
+                        )}
+                      </div>
+                      <span>{emotion.title}</span>
+                    </div>
+                  ))}
+                </div>
+              )}
+              <ErrorMessage
+                name="emotions"
+                component="div"
+                className={css.error}
+              />
+            </div>
+
+            <div className={css.fieldWrap}>
+              <label className={css.title} htmlFor="description">
+                Запис
+              </label>
+              <Field
+                className={`${css.textarea} ${errors.description && touched.description ? css.inputError : ""}`}
+                as="textarea"
+                name="description"
+                id="description"
+                rows={5}
+                placeholder="Запишіть, як ви себе відчуваєте"
+              />
+              <ErrorMessage
+                name="description"
+                component="div"
+                className={css.error}
+              />
+            </div>
+
+            <button
+              className={css.button}
+              type="submit"
+              disabled={isSubmitting}
+            >
+              {isSubmitting
+                ? isEditing
+                  ? "Оновлення..."
+                  : "Збереження..."
+                : "Зберегти"}
+            </button>
+          </Form>
         )}
       </Formik>
     </>
