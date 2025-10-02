fcd4e7e753b202a4395c779a59e0efcc8072a690
Danylo Liubota <daniil.lyubota@gmail.com>
2025-10-02 18:42:13 +0300

    fix: fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,22 +3,22 @@ import localFont from "next/font/local";
 export const lato = localFont({
   src: [
     {
-      path: "../public/fonts/Lato/Lato-Regular.woff2",
+      path: "../public/fonts/lato/Lato-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../public/fonts/Lato/Lato-Bold.woff2",
+      path: "../public/fonts/lato/Lato-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../public/fonts/Lato/Lato-Medium.woff2",
+      path: "../public/fonts/lato/Lato-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../public/fonts/Lato/Lato-Semibold.woff2",
+      path: "../public/fonts/lato/Lato-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
