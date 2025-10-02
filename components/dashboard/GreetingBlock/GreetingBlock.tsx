a184bbd9cfe5375cccc3421033f9c794343667c3
Danylo Liubota <daniil.lyubota@gmail.com>
2025-10-02 15:51:45 +0300

    fix: update username

--- components/dashboard/GreetingBlock/GreetingBlock.tsx
+++ components/dashboard/GreetingBlock/GreetingBlock.tsx
@@ -4,12 +4,12 @@ import css from "./GreetingBlock.module.css";
 import { useEffect, useState } from "react";
 import { useAuthStore } from "@/lib/store/authStore";
 
-function getGreeting(date = new Date) {
+function getGreeting(date = new Date()) {
   const hour = date.getHours();
   if (hour >= 6 && hour < 12) return "Доброго ранку";
   if (hour >= 12 && hour < 18) return "Доброго дня";
   if (hour >= 18 && hour < 24) return "Доброго вечора";
-  return "Доброї ночі"
+  return "Доброї ночі";
 }
 
 export default function GreetingBlock() {
@@ -20,8 +20,14 @@ export default function GreetingBlock() {
   useEffect(() => {
     if (isAuthenticated && user?.name) {
       setUserName(user?.name);
+    } else {
+      setUserName("мама");
     }
   }, [isAuthenticated, user?.name]);
 
-  return <h1 className={css.title}>{greeting}, {userName}!</h1>;
+  return (
+    <h1 className={css.title}>
+      {greeting}, {userName}!
+    </h1>
+  );
 }
